706e14687c503a9d69adbe259067e5f1e90aa656
Glissy McGowan <glissy.mcgowan@gmail.com>
2015-11-28 13:38:33 -0600

    updated license

--- smartapps/mobious/routine-execute-triggers-open-door-check.src/routine-execute-triggers-open-door-check.groovy
+++ smartapps/mobious/routine-execute-triggers-open-door-check.src/routine-execute-triggers-open-door-check.groovy
@@ -1,8 +1,18 @@
 /**
- *  Routine Triggers Open Door Check
- *
  *  Copyright 2015 Glen McGowan
  *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Routine Execute Triggers Open Door Check
+ *
+ *  Author: Glen McGowan
  */
 definition(
     name: "Routine Execute Triggers Open Door Check",
@@ -75,7 +85,7 @@ def checkDoor() {
     def list = open.size() > 1 ? "are" : "is"
     if (open) {
     	//format the list and push it.
-		def message = "Secuirty Check Failed: ${open.join(', ')} ${list} open"
+		def message = "Security Check Failed: ${open.join(', ')} ${list} open"
     	log.info message
         //Hard code push notification for now. 
         if (sendPush) {
